c77933380be13ddc10b2e94e15c62512bd747708
Ivan <proekt.energetika@gmail.com>
2018-11-23 18:42:05 +0300

    Текущее состояние раздела consultants

--- source/js/slider-init.js
+++ source/js/slider-init.js
@@ -6,12 +6,37 @@
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
-      lazyLoad: 'ondemand',
       dots: true,
       dotsClass: 'custom_paging',
       customPaging: function (slider, i) {
-          return  (i + 1) + '  /  ' + slider.slideCount;
+        return  (i + 1) + '  /  ' + slider.slideCount;
       }
+    });
+  });
+
+  $(document).ready(function(){
+    $('.consultants__list').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      dots: false,
+      adaptiveHeight: true,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 1279,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
     });
   });
 })();
